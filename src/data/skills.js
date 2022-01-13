ea8757bba129fd2920feb0a3c63c036f0df2856e
xmudronc <44iwan44@gmail.com>
2022-01-13 23:57:58 +0100

    First idea of save file format.

---
+++ src/data/skills.js
@@ -0,0 +1,8 @@
+const skills = [
+    {
+        id: 0,
+        name: "skill of add 2 to str when dex >= 10",
+        description: "yep",
+        bonus: 2
+    },
+]
---
+++ src/data/weapons.js
@@ -0,0 +1,8 @@
+const weapons = [
+    {
+        id: 0,
+        name: "sword of add 2 to str",
+        description: "yep",
+        bonus: 2
+    },
+]
